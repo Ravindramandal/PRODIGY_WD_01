50f5832c0fcb1d67b996a9fe7b36e7ba4ab3ab91
Ravindramandal <mandalravindra147@gmail.com>
2024-05-16 22:33:52 +0530

    Delete PRODIGY directory

--- PRODIGY/index.js
+++
@@ -1,9 +0,0 @@
-let image=document.querySelector(".image");
-function phones(phone){
-    image.src =phone;
-}
-let x=document.querySelector(".contain");
-function changeColor(color){
-    
-    x.style.background=color;
-}
